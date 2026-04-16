8469348c0b8cd3ba8cb29d4fde52c134bdc0d5de
Dino Besic <zmaj@MBP-od-Dino.fritz.box>
2026-04-16 12:00:09 +0200

    fix language selecotr

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,4 +1,4 @@
-export { B as handler, C as options, D as startServer } from './chunks/server_CKeCO4Jz.mjs';
+export { B as handler, C as options, D as startServer } from './chunks/server_Wq5ydqW4.mjs';
 import '@astrojs/internal-helpers/path';
 import '@astrojs/internal-helpers/remote';
 import 'piccolore';
