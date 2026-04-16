4cba072651a0b9da87a10412b65d0dabdcaa91eb
Dino Besic <zmaj@MBP-od-Dino.fritz.box>
2026-04-16 05:36:28 +0200

    perf: self-hosted google fonts and optimized js payload

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,4 +1,4 @@
-export { B as handler, C as options, D as startServer } from './chunks/server_LdCZm9Mz.mjs';
+export { B as handler, C as options, D as startServer } from './chunks/server_C948CP2d.mjs';
 import '@astrojs/internal-helpers/path';
 import '@astrojs/internal-helpers/remote';
 import 'piccolore';
